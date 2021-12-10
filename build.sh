4a320acc3d7eaa61791155ca05b8a7b134d94619
Olivier Korach <olivier.korach@gmail.com>
2021-12-10 13:59:56 +0100

    Up 1 level

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+echo "Running: \
+    Coverage tests
+"
+./run_tests.sh
 
-# Run each build.sh script in underlying comp-* directories
-for d in *; do
-    if [[ -d $d ]] && [[ $d =~ comp-.* ]]
-    then
-        cd $d; ./build.sh $*; cd -
-    fi
-done
+echo "Running: \
+    3rd part linters
+"
+./run_linters.sh
+
+echo "Running: \
+    sonar-scanner $* 
+"
+sonar-scanner $*
